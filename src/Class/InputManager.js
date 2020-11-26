4803cd5c030dd909d0312e524557b791f2762005
AnibalDBXD <olivaresluisanibal@gmail.com>
2020-11-26 16:17:40 -0400

    Inputmanager was added to add the search function, Responsive was also added

---
+++ src/Class/InputManager.js
@@ -0,0 +1,25 @@
+export default class InputManager {
+  constructor(renderFunction) {
+    this.render = renderFunction;
+
+    this.search_input = document.getElementById("search_input");
+    this.search_button = document.getElementById("search_button");
+
+    this.location_input = document.getElementById("location_input");
+
+    this.search_button.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.render();
+    });
+    this.search_input.addEventListener("keyup", (e) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        this.render();
+      }
+    });
+  }
+
+  getQuery() {
+    return this.search_input.value;
+  }
+}
--- src/Class/JobsContainer.js
+++ src/Class/JobsContainer.js
@@ -1,29 +1,35 @@
 ("use strict");
 
-import CheckboxManager from "./CheckboxManager.js";
-import Job from "./Job.js";
-
 import FetchAPI from "../api/FetchAPI.js";
 import config from "../api/config.js";
 
+import CheckboxManager from "./CheckboxManager.js";
+import Job from "./Job.js";
+import InputManager from "./InputManager.js";
+
 export default class JobsContainer {
   constructor() {
-    this.Input = "";
     this.jobs_container = document.getElementById("jobs-container");
+    this.InputManager = new InputManager(this.render.bind(this));
     this.CheckboxManager = new CheckboxManager(
       this.render.bind(
         this
       ) /* bind() is to anchor the context of this object to the CheckboxManager */
     );
-    this.FetchAPI = new FetchAPI(config.URL + "description=&location=");
+    this.FetchAPI = new FetchAPI(
+      config.URLWithoutCors + "description=&location="
+    );
   }
 
   render() {
+    const ActiveCheckBoxes = this.CheckboxManager.CheckBoxesToString(
+      this.CheckboxManager.getActiveCheckBoxes()
+    );
+    const Search_query = this.InputManager.getQuery();
+
     this.FetchAPI.changeURL(
-      config.URL +
-        `description=&location=${this.CheckboxManager.CheckBoxesToString(
-          this.CheckboxManager.getActiveCheckBoxes()
-        )}`
+      config.URLWithoutCors +
+        `search=${Search_query}&location=${ActiveCheckBoxes}`
     );
     this.postJob();
   }
@@ -33,12 +39,13 @@ export default class JobsContainer {
   }
 
   postJob() {
-    this.jobs_container.innerHTML = "";
-    this.FetchAPI.getData().then((result) =>
+    this.jobs_container.innerHTML = "<h1>Loading...</h1>";
+    this.FetchAPI.getData().then((result) => {
+      this.jobs_container.innerHTML = "";
       result.forEach(
         (job) =>
           (this.jobs_container.innerHTML += this.createJob(job).createBadge())
-      )
-    );
+      );
+    });
   }
 }
